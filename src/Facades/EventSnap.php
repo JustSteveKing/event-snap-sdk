75eebd098d99be76590c1314f19aae0d1d014119
Steve McDougall <juststevemcd@gmail.com>
2023-06-14 12:08:17 +0100

    Updating facade docblock

--- src/Facades/EventSnap.php
+++ src/Facades/EventSnap.php
@@ -6,6 +6,10 @@ namespace EventSnap\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void send(string $name, string $icon, string $description, string $channel, string $project, array $tags = [], bool $notify = false)
+ * @see \EventSnap\Http\EventSnap
+ */
 final class EventSnap extends Facade
 {
     protected static function getFacadeAccessor(): string
